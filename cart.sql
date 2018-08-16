3648fe6af742966e4469cac626d2b3bc13782708
Your Name <you@example.com>
2018-08-16 15:54:09 +0800

    sql + insert into

--- cart.sql
+++ cart.sql
@@ -5,12 +5,21 @@ create table product
   price float        null
 );
 
+insert into product values (1, '黑色的丝袜', 500);
+insert into product values (2, '充气娃娃', 2500);
+insert into product values (3, '皮鞭', 180);
+insert into product values (4, '蜡烛', 0.20);
+
+
 create table user
 (
   id       int         null,
   name     varchar(10) null,
   password varchar(50) null
 );
+
+insert into user values (1, 'admin', 'root');
+
 
 create table order_ (
   id  int AUTO_INCREMENT,
